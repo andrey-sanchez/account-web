3f8c13464a2250cda51d1d20508f032ef0b48659
angrifel <angrifel@users.noreply.github.com>
2018-08-07 14:03:40 -0300

    added primary key creation

--- src/main/java/com/bankito/account/spark/route/RecreateDbRoute.java
+++ src/main/java/com/bankito/account/spark/route/RecreateDbRoute.java
@@ -22,12 +22,9 @@ public class RecreateDbRoute implements Route {
       handler.execute("DROP TABLE IF EXISTS transaction");
       handler.execute("DROP TABLE IF EXISTS movement");
       handler.execute("DROP TABLE IF EXISTS account");
-      handler.execute("CREATE TABLE account (id NVARCHAR(32) NOT NULL, owner NVARCHAR(64) NOT NULL)");
-      handler.execute("CREATE TABLE movement (id NVARCHAR(32) NOT NULL, account_id NVARCHAR(32) NOT NULL, transaction_id NVARCHAR(32) NOT NULL, amount BIGINT NOT NULL, timestamp BIGINT NOT NULL, description NVARCHAR(128) NOT NULL)");
-      handler.execute("CREATE TABLE transaction (id NVARCHAR(32) NOT NULL, owner NVARCHAR(64) NOT NULL, timestamp bigint NOT NULL, type VARCHAR(16) NOT NULL)");
-      handler.execute("SELECT * FROM account WHERE id = ''");
-      handler.execute("SELECT * FROM movement WHERE id = ''");
-      handler.execute("SELECT * FROM transaction WHERE id = ''");
+      handler.execute("CREATE TABLE account (id NVARCHAR(32) NOT NULL PRIMARY KEY, owner NVARCHAR(64) NOT NULL)");
+      handler.execute("CREATE TABLE movement (id NVARCHAR(32) NOT NULL PRIMARY KEY, account_id NVARCHAR(32) NOT NULL, transaction_id NVARCHAR(32) NOT NULL, amount BIGINT NOT NULL, timestamp BIGINT NOT NULL, description NVARCHAR(128) NOT NULL)");
+      handler.execute("CREATE TABLE transaction (id NVARCHAR(32) NOT NULL PRIMARY KEY, owner NVARCHAR(64) NOT NULL, timestamp bigint NOT NULL, type VARCHAR(16) NOT NULL)");
 
     });
 
